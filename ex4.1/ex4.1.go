e28a60410d288c1cd3c5369144e4feff2dfed924
parkforte <parkforte@naver.com>
2022-05-06 08:12:32 +0900

    ex4.1 variable

---
+++ ex4.1/ex4.1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	//var 변수를 사용하겠다  라는 의미
+	var msg string = "Hello Variable"
+
+	a = 20
+	msg = "Good Morning"
+	fmt.Println(msg, a)
+}
