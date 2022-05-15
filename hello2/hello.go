26ce553ca0c1be1ea343d9283b1479f4aa9c6bc1
parkforte <parkforte@naver.com>
2022-05-15 22:36:26 +0900

    commit

---
+++ hello2/hello.go
@@ -0,0 +1,18 @@
+package main
+
+//main은 프로그램의 시작점을 의미
+//프로그램을 시작하는 package라는 의미
+//go는 모든 코드가 package에 포함되어야 한다.
+
+import "fmt"
+
+//패키지를 가져오는 것
+
+func main() {
+
+	/*
+		여러줄주석
+	*/
+
+	fmt.Println("hello go world")
+}
